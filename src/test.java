81c8317f506584c387a425691743e3d87213bc7e
ryanzhxu <ryanzhxu@students.cs.ubc.ca>
2021-06-28 15:49:23 -0700

    implemented plusOne algorithm which adds 1 to a number stored as array.

--- src/test.java
+++ src/test.java
@@ -1,5 +1,21 @@
 public class test {
-    private static int[] foo(int[] arr) {
+
+    private static int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i=n-1; i>=0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] arr = new int[n+1];
+        arr[0] = 1;
+
+        return arr;
+    }
+
+    private static int[] productExceptIdx(int[] arr) {
         int[] result = new int[arr.length];
         int product = 1;
         for (int j : arr) {
@@ -11,14 +27,16 @@ public class test {
         return result;
     }
 
+    private static void printArray(int[] arr) {
+        for (int i: arr) {
+            System.out.print(i + " ");
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World");
-        int[] arr = {1, 2, 3, 4};
-        int[] res = foo(arr);
-        for (int re : res) {
-//            System.out.println("res[" + i + "] = " + res[i]);
-            System.out.print(re + " ");
-        }
+        int[] arr = {9, 9, 9, 9};
+        printArray(plusOne(arr));
     }
 
 }
